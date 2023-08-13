47e9988c85283d50b3a161b2a0974661b829ad80
n2n <askprogrammers@gmail.com>
2023-08-12 20:36:09 -0400

    tiny test

--- README.md
+++ README.md
@@ -1,29 +1,14 @@
 # llm_vs_vector
-Made a little script to speed and cost of classification via LLM or via vector embeddings
+Source: <a href="http://yoheinaka](https://twitter.com/yoheinakajima/status/1688032436788322304" target="_blank">Original tweet thread</a> about this.
 
-Currently tests the classification of 50 sentences into positive or negative using three approaches:
-- ChatCompletion [gpt-3.5-turbo]
-- Comparing vector embeddings (to positive and negative) [text-embedding-ada-002]
-- Comparing vector embeddings (to positive and negative) [spacy]
-
-Stats Tracked:
-- Speed is tracked for all three methods.
-- Distance to positive and negative is tracked for vector embedding methods
-- Token count and cost is tracked for ChatCompletion and ada-002 vector embedding.
-
-Here's the <a href="http://yoheinaka](https://twitter.com/yoheinakajima/status/1688032436788322304" target="_blank">Original tweet thread</a> about this.
-
-Update 1: Added multi.py for testing classification into more than two options, using movies and movie genres (only 3.5 and ada)
-
-Example output:
-<img src="https://pbs.twimg.com/media/F21AulEb0AAPC5A?format=jpg&name=4096x4096" alt="Example output">
-
-
+Output:
+![Alt text](image.png)
 
 ## Setup
+`venv it`
 `pip install -r requirements.txt`
 `python -m spacy download en_core_web_md`
-`export OPENAI_API_KEY=<your key here>`
+`drop your key in`
 
 ## Usage
 `python main.py`
--- main.py
+++ main.py
@@ -22,15 +22,20 @@ def num_tokens_from_string(string: str, encoding_name: str) -> int:
 nlp = spacy.load("en_core_web_md")
 
 
-positive_sentiment_base = "Positive:  Joyful Elated Ecstatic Content Jubilant Optimistic Serene Euphoric Radiant Thrilled Positive Bliss Elation Jubilation Serenity Triumph Delight Exuberance Harmony Reverie Zenith"
-negative_sentiment_base = "Negative:  Despondent Morose Disheartened Forlorn Melancholic Pessimistic Dismayed Frustrated Anguished Apprehensive Negative Despair Gloom Dismay Angst Malaise Turmoil Woe Heartbreak Affliction Abyss"
+# positive_sentiment_base = "Positive:  Joyful Elated Ecstatic Content Jubilant Optimistic Serene Euphoric Radiant Thrilled Positive Bliss Elation Jubilation Serenity Triumph Delight Exuberance Harmony Reverie Zenith"
+# negative_sentiment_base = "Negative:  Despondent Morose Disheartened Forlorn Melancholic Pessimistic Dismayed Frustrated Anguished Apprehensive Negative Despair Gloom Dismay Angst Malaise Turmoil Woe Heartbreak Affliction Abyss"
+
+swap_intent_base = "Swap intent: convert, exchange, trade, flip, transmute, interchange, replace"
+send_intent_base = "Send intent: transfer, send to, move, route, forward, pass, pay, remit"
 
 # Embed the words "positive" and "negative" using spaCy
-positive_embedding_spacy = nlp(positive_sentiment_base).vector
-negative_embedding_spacy = nlp(negative_sentiment_base).vector
+swap_embedding_spacy = nlp(swap_intent_base).vector
+send_embedding_space = nlp(send_intent_base).vector
+def spacy_similarity(sentence, base_text):
+    return nlp(sentence).similarity(nlp(base_text))
 
 # Set up the OpenAI API
-openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = print('your key here')
 
 
 # Function to get embeddings using OpenAI's API
@@ -48,155 +53,107 @@ def get_ada_embedding(text):
 
 
 
+swap_embedding_ada = get_ada_embedding(swap_intent_base)
+send_embedding_ada = get_ada_embedding(send_intent_base)
 
-# Get embeddings for "positive" and "negative" using ADA
-positive_embedding_ada = get_ada_embedding(positive_sentiment_base)
-negative_embedding_ada = get_ada_embedding(negative_sentiment_base)
 
 # Example sentences
 sentences = [
-  "Tears streamed down her face as she read the letter from home.",
-  "In the eerie silence of the haunted house, every creak echoed.",
-  "His heart swelled with pride watching his daughter's graduation.",
-  "The intensity of the storm outside mirrored her turbulent emotions.",
-  "He sighed with relief as he found his lost keys in the couch.",
-  "In the glow of the campfire, they exchanged stories of their childhood.",
-  "The grim news on the radio cast a shadow over their celebrations.",
-  "Her infectious laughter turned a mundane day into a memorable one.",
-  "At the summit, overwhelmed by the view, he felt insignificantly small.",
-  "The suspenseful climax of the movie had everyone on the edge of their seats.",
-  "Lost in the maze of alleyways, panic began to set in.",
-  "The first rays of sunrise brought hope to the weary travelers.",
-  "A shiver ran down his spine as he recalled the ghostly tale.",
-  "The festive atmosphere in the market was contagious, uplifting everyone's spirits.",
-  "His eyes brimmed with unshed tears as he bid farewell to his family.",
-  "The aroma of her grandmother's cookies evoked a flood of memories.",
-  "Their argument escalated, filling the room with tension.",
-  "In the tranquil meadow, she felt a deep connection to nature.",
-  "The haunting melody of the song stirred something deep within him.",
-  "The chaotic aftermath of the earthquake was a sight of devastation.",
-  "With bated breath, they awaited the verdict of the competition.",
-  "Amidst the ruins, he found a photo that spoke of happier times.",
-  "The spontaneous dance in the rain rejuvenated her spirit.",
-  "In the dark alley, the menacing shadow loomed closer.",
-  "The comedian's joke had the entire hall erupting in laughter.",
-  "She felt a twinge of envy seeing her friend's success.",
-  "Amidst the hustle and bustle of the city, the park was an oasis.",
-  "The weight of his guilt was palpable in the room.",
-  "The beauty of the meteor shower left them in awestruck wonder.",
-  "The news of the accident cast a pall of gloom over the village.",
-  "His gentle reassurance dispelled her fears.",
-  "The scandal sent shockwaves throughout the community.",
-  "In the golden hue of sunset, the world seemed to pause.",
-  "The emptiness of the house echoed her loneliness.",
-  "His surprise proposal at the concert was met with cheers and applause.",
-  "The foreboding forest hid secrets in its shadows.",
-  "Her joy at reuniting with her long-lost friend was palpable.",
-  "The tense standoff at the border was a geopolitical powder keg.",
-  "The gentle hum of the bees was a lullaby to her afternoon nap.",
-  "In the heat of the moment, words were exchanged that couldn't be taken back.",
-  "The unveiling of the monument was a moment of national pride.",
-  "As the ship sank, a somber silence gripped the onlookers.",
-  "She blushed as the room burst into applause for her performance.",
-  "The inexplicable events at the mansion were the talk of the town.",
-  "The joyride took a dark turn as the storm clouds gathered.",
-  "In the glow of the lanterns, the festival came alive.",
-  "The sudden betrayal was a bitter pill to swallow.",
-  "The symphony of nature at dawn was pure magic.",
-  "His anxiety was evident as he paced the waiting room.",
-  "The mural spoke of a bygone era, filled with romance and rebellion."
+  "Put my balance into eth",
+  "send this to my friend",
+  "i want btc out of my eth"
+  "transfer 10 eth to prala",
+  "let's go all in on eth",
+  "i want to buy eth",
+  "put it all in eth",
+  "give it to langwallet.eth",
+  "send 10 eth to langwallet.eth",
+  "pass it to langwallet.eth",
 ]
 
 results = []
+spacy_results = []
 
 for sentence in sentences:
-  print(sentence)
-  start_time = time.time()
+    print(sentence)
+    # Time measurement for GPT-3.5
+    start_time_gpt = time.time()
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{
+            "role": "system",
+            "content": "You are a helpful assistant that only responds as 'swap' or 'send'."
+        }, {
+            "role": "user",
+            "content": f"Is the following user intent to swap or send: (options:swap/send)?\n\n{sentence}"
+        }])
+    davinci_duration = time.time() - start_time_gpt
+    davinci_classification = response.choices[0].message['content'].strip().lower()
+    print('gpt says', davinci_classification)
 
-  # Classification using ChatCompletion API
-  response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[{
-      "role":
-      "system",
-      "content":
-      "You are a helpful assistant that only responds as 'positive' or 'negative'."
-    }, {
-      "role":
-      "user",
-      "content":
-      f"Is the following sentence positive or negative (options:positive/negative)?\n\n{sentence}"
-    }])
-  davinci_duration = time.time() - start_time
-  davinci_classification = response.choices[0].message['content'].strip(
-  ).lower()
-  print(davinci_classification)
 
-  start_time = time.time()
-  # Classification using spaCy embeddings
-  sentence_embedding = nlp(sentence).vector
-  pos_distance_spacy = distance.cosine(sentence_embedding, positive_embedding_spacy)
-  neg_distance_spacy = distance.cosine(sentence_embedding, negative_embedding_spacy)
-  embedding_duration_spacy = time.time() - start_time
-  if pos_distance_spacy < neg_distance_spacy:
-    embedding_classification_spacy = "positive"
-  else:
-    embedding_classification_spacy = "negative"
-  print(embedding_classification_spacy)
-  # Classification using ADA embeddings
-  start_time = time.time()
-  sentence_embedding_ada = get_ada_embedding(sentence)
-  pos_distance_ada = distance.cosine(sentence_embedding_ada, positive_embedding_ada)
-  neg_distance_ada = distance.cosine(sentence_embedding_ada, negative_embedding_ada)
-  embedding_duration_ada = time.time() - start_time
-  if pos_distance_ada < neg_distance_ada:
-    embedding_classification_ada = "positive"
-  else:
-    embedding_classification_ada = "negative"
-  print(embedding_classification_ada)
 
-  tokens_gpt_3_5 = num_tokens_from_string(sentence, "cl100k_base")
-  tokens_ada = num_tokens_from_string(sentence,"cl100k_base")  # Assuming the same tokenization method
+    start_time_spacy = time.time()
+    swap_similarity_spacy = spacy_similarity(sentence, swap_intent_base)
+    send_similarity_spacy = spacy_similarity(sentence, send_intent_base)
+    
+    if swap_similarity_spacy > send_similarity_spacy:
+        embedding_classification_spacy = "swap"
+    else:
+        embedding_classification_spacy = "send"
+        
+    spacy_duration = time.time() - start_time_spacy
+    
+    spacy_results.append((embedding_classification_spacy, spacy_duration))
+    
+    # Time measurement for ADA embeddings
+    start_time_ada = time.time()
+    sentence_embedding_ada = get_ada_embedding(sentence)
+    swap_distance_ada = distance.cosine(sentence_embedding_ada, swap_embedding_ada)
+    send_distance_ada = distance.cosine(sentence_embedding_ada, send_embedding_ada)
+    if swap_distance_ada < send_distance_ada:
+        embedding_classification_ada = "swap"
+    else:
+        embedding_classification_ada = "send"
+    ada_duration = time.time() - start_time_ada
 
-  cost_gpt_3_5 = tokens_gpt_3_5 * TOKEN_COST_GPT_3_5
-  cost_ada = tokens_ada * TOKEN_COST_ADA
+    tokens_gpt_3_5 = num_tokens_from_string(sentence, "cl100k_base")
+    tokens_ada = num_tokens_from_string(sentence,"cl100k_base")  # Assuming the same tokenization method
 
-  # Storing results
-  results.append(
-      (davinci_classification, davinci_duration, tokens_gpt_3_5, cost_gpt_3_5,
-       embedding_classification_spacy, pos_distance_spacy, neg_distance_spacy, embedding_duration_spacy,
-       embedding_classification_ada, pos_distance_ada, neg_distance_ada, embedding_duration_ada, tokens_ada, cost_ada)
-  )
+    cost_gpt_3_5 = tokens_gpt_3_5 * TOKEN_COST_GPT_3_5
+    cost_ada = tokens_ada * TOKEN_COST_ADA
+
+    results.append((davinci_classification, davinci_duration, tokens_gpt_3_5, cost_gpt_3_5,
+                    embedding_classification_ada, swap_distance_ada, send_distance_ada, ada_duration, tokens_ada, cost_ada))
+
 
 # Print results
-header = "| Sentence | gpt-3.5 Class. | gpt-3.5 Time | gpt-3.5 Tokens | gpt-3.5 Cost | spaCy Class. | spaCy Dist. Pos. | spaCy Dist. Neg. | spaCy Time | ada-002 Class. | ada Dist. Pos. | ada Dist. Neg. | ada-002 Time | ada-002 Tokens | ada-002 Cost |"
-separator = "|----------|---------------|--------------|---------------|--------------|--------------|-----------------|-----------------|------------|----------------|----------------|----------------|--------------|---------------|--------------|"
+header = "| Sentence | gpt-3.5 Class. | gpt-3.5 Time | gpt-3.5 Tokens | gpt-3.5 Cost | ada-002 Class. | ada Dist. Swap | ada Dist. Send | ada-002 Time | ada-002 Tokens | ada-002 Cost |"
+separator = "|----------|---------------|--------------|---------------|--------------|----------------|----------------|----------------|--------------|---------------|--------------|"
+header += "| spaCy Class. | spaCy Time |"
+separator += "|--------------|-----------|"
+print(header)
+print(separator)
 
-print("")
 print(header)
 print(separator)
 for i, (davinci_classification, davinci_duration, davinci_tokens, cost_gpt_3_5,
-        embedding_classification_spacy, pos_distance_spacy, neg_distance_spacy, embedding_duration_spacy,
-        embedding_classification_ada, pos_distance_ada, neg_distance_ada, embedding_duration_ada, ada_tokens, cost_ada) in enumerate(results):
+        embedding_classification_ada, swap_distance_ada, send_distance_ada, embedding_duration_ada, ada_tokens, cost_ada) in enumerate(results):
+    
+    ada_color = '\033[94m' if 'swap' in embedding_classification_ada else '\033[93m'
 
-    davinci_color = '\033[92m' if 'positive' in davinci_classification else '\033[91m'
-    spacy_color = '\033[92m' if 'positive' in embedding_classification_spacy else '\033[91m'
-    ada_color = '\033[92m' if 'positive' in embedding_classification_ada else '\033[91m'
-
+    spacy_classification, spacy_duration = spacy_results[i]
+    
     print(
-        f"| {sentences[i][:30]}.. | {davinci_color}{davinci_classification}\033[0m | {davinci_duration:.2f} sec | {davinci_tokens} tokens | ${cost_gpt_3_5:.2f} | {spacy_color}{embedding_classification_spacy}\033[0m | {pos_distance_spacy:.3f} | {neg_distance_spacy:.3f} | {embedding_duration_spacy:.2f} sec | {ada_color}{embedding_classification_ada}\033[0m | {pos_distance_ada:.3f} | {neg_distance_ada:.3f} | {embedding_duration_ada:.2f} sec | {ada_tokens} tokens | ${cost_ada:.2f} |"
+        f"| {sentences[i][:35]}.. | {davinci_classification} | {davinci_duration:.2f} sec | {davinci_tokens} tokens | ${cost_gpt_3_5:.2f} | {embedding_classification_ada} | {swap_distance_ada:.3f} | {send_distance_ada:.3f} | {embedding_duration_ada:.2f} sec | {ada_tokens} tokens | ${cost_ada:.2f} | {spacy_classification} | {spacy_duration:.2f} sec |"
     )
-
 # Average results
 avg_davinci_time = sum([res[1] for res in results]) / len(results)
-avg_spacy_time = sum([res[7] for res in results]) / len(results)
-avg_ada_time = sum([res[11] for res in results]) / len(results)
-print("")
-# Average costs
 avg_gpt_3_5_cost = sum([res[3] for res in results]) / len(results)
-avg_ada_cost = sum([res[13] for res in results]) / len(results)
+avg_ada_time = sum([res[7] for res in results]) / len(results)
+avg_ada_cost = sum([res[9] for res in results]) / len(results)
+avg_spacy_time = sum([res[1] for res in spacy_results]) / len(spacy_results) #cost = free
 
-# ...
 
 print("\nAverage gpt-3.5 Time:", avg_davinci_time)
 print("Average gpt-3.5 Cost: $",avg_gpt_3_5_cost)
